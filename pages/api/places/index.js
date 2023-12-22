edb5fbf89f16d28bfedcb4d31203be4062a186da
onur fistikci <onurfistikci@mail.de>
2023-12-22 16:18:19 +0100

    main

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -4,9 +4,14 @@ import Place from "@/db/models/Place";
 export default async function handler(request, response) {
   await dbConnect();
 
+  // the HTTP method "GET" to retrieve data from our server
+  // the request is here so that the data from the server gets on the http://localhost:3000/ page 
   if (request.method === "GET") {
     const places = await Place.find();
     return response.status(200).json(places);
+
+  // the HTTP method "PUT" to update/edit data on our server
+  // the request is here so that can be created and gets schown on the http://localhost:3000/ page 
   } else if (request.method === "POST") {
     try {
       const placeData = request.body;
