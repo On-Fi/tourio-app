8959713bad62e72daf9d13693f5cc235393bce4c
onur fistikci <onurfistikci@mail.de>
2023-12-22 15:45:12 +0100

    app is done

---
+++ next.config.js
@@ -0,0 +1,42 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "unsplash.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "commons.wikimedia.org",
+        port: "",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
---
+++ pages/api/places/index.js
@@ -0,0 +1,23 @@
+import dbConnect from "@/db/connect";
+import Place from "@/db/models/Place";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const places = await Place.find();
+    return response.status(200).json(places);
+  } else if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      await Place.create(placeData);
+
+      response.status(201).json({ status: "Place added" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
+}
